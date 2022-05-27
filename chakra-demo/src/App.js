df8c23d42e88bf6a655eca977bb47abbfb082fd2
Azmain Adel <azmainadel47@gmail.com>
2022-05-27 06:36:21 +0000

    :art: Create template components

---
+++ chakra-demo/src/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import { ChakraProvider, Box, VStack, Grid, theme } from '@chakra-ui/react';
+import { CallToActionWithAnnotation } from './component/hero/CallToActionWithAnnotation';
+import { WithSpeechBubbles } from './component/testimonial/WithSpeechBubbles';
+import { Footer } from './component/footer/Footer';
+import { ColorModeSwitcher } from './utility/ColorModeSwitcher';
+import { Story } from './component/story/Story';
+
+function App() {
+  return (
+    <ChakraProvider theme={theme}>
+      <Box textAlign="center" fontSize="xl">
+        <Grid minH="100vh" p={3}>
+          <ColorModeSwitcher justifySelf="flex-end" />
+
+          <VStack spacing={8}>
+            <CallToActionWithAnnotation />
+          </VStack>
+        </Grid>
+        <WithSpeechBubbles />
+        <Story />
+        <Footer />
+      </Box>
+    </ChakraProvider>
+  );
+}
+
+export default App;
