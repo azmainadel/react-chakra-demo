c6c6f2606ea492cb8da43ae9accb12306495b2a9
Azmain Adel <azmainadel47@gmail.com>
2022-05-30 06:03:59 +0000

    :zap: Add Navar

--- src/App.js
+++ src/App.js
@@ -5,20 +5,21 @@ import { WithSpeechBubbles } from './component/testimonial/WithSpeechBubbles';
 import { Footer } from './component/footer/Footer';
 import { ColorModeSwitcher } from './utility/ColorModeSwitcher';
 import { Story } from './component/story/Story';
+import WithSubnavigation from './component/navbar/WithSubnavigation';
 
 function App() {
   return (
     <ChakraProvider theme={theme}>
+      <WithSubnavigation/>
       <Box textAlign="center" fontSize="xl">
         <Grid minH="100vh" p={3}>
           <ColorModeSwitcher justifySelf="flex-end" />
-
           <VStack spacing={8}>
             <CallToActionWithAnnotation />
           </VStack>
         </Grid>
         <WithSpeechBubbles />
-        <Story />
+        {/* <Story /> */}
         <Footer />
       </Box>
     </ChakraProvider>
